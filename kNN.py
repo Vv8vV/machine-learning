9b7cafe8e9df9847139ab083dfb52901f2344ebb
sheng liang <hardrock1937@hotmail.com>
2014-12-03 16:24:29 +0800

    kkk1

--- kNN.py
+++ kNN.py
@@ -109,4 +109,4 @@ def handwritingClassTest():
             print fileStr
             errorCount += 1.0
     print "\nthe total number of errors is: %d" % errorCount
-    print "\nthe total error rate　 is: %f" % (errorCount/float(mTest))
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
