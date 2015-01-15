1e3c5db351c74e2b4a66322b7dc5ff487cda90d0
sheng liang <hardrock1937@hotmail.com>
2015-01-15 15:33:54 +0800

    svm

--- logRegres.py
+++ logRegres.py
@@ -68,6 +68,59 @@ def stocGradAscent0(dataMatrix, classLabels):
 		weights = weights + alpha * error * dataMatrix[i]
 	return weights
 
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+	m,n = shape(dataMatrix)
+	alpha = 0.01
+	weights = ones(n)
+	for j in range(numIter):
+		dataIndex = range(m)
+		for i in range(m):
+			alpha = 4/(1.0+j+1) + 0.01
+			randIndex = int(random.uniform(0, len(dataIndex)))
+			h = sigmoid(sum(dataMatrix[randIndex]*weights))
+			error = classLabels[randIndex] - h
+			weights = weights + alpha * error * dataMatrix[randIndex]
+			del(dataIndex[randIndex])
+	return weights
+
+def classifyVector(inX, weights):
+	prob = sigmoid(sum(inX*weights))
+	if prob > 0.5:
+		return 1.0
+	else:
+		return 0.0
+
+def colicTest():
+	frTrain = open("data\\Ch05\\horseColicTraining.txt")
+	frTest = open("data\\Ch05\\horseColicTest.txt")
+	trainingSet = []
+	trainingLabels = []
+	for line in frTrain.readlines():
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		trainingSet.append(lineArr)
+		trainingLabels.append(float(currLine[21]))
+
+	trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
+	
+	errorCount = 0
+	numTestVec = 0.0
+	for line in frTest.readlines():
+		numTestVec += 1.0
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+			errorCount += 1
+	errorRate = (float(errorCount)/numTestVec)
+	print "the error rate of this test is: %f" % errorRate
+	return errorRate
+			
+
+
 def testAscent():
 	dataMat, labelMat = loadDataSet()
 	weights = gradAscent(dataMat, labelMat)
@@ -77,3 +130,16 @@ def testStocAscent0():
 	dataMat, labelMat = loadDataSet()
 	weights = stocGradAscent0(array(dataMat), labelMat)
 	plotBestFit(weights)
+
+def testStocAscent1():
+	dataMat, labelMat = loadDataSet()
+	weights = stocGradAscent1(array(dataMat), labelMat)
+	plotBestFit(weights)
+
+def multiTest():
+	numTests = 10
+	errorSum = 0
+	for k in range(numTests):
+		errorSum += colicTest()
+	print "After %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
+	
